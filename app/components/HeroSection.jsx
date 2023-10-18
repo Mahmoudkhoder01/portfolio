ebd9cf250449956b80dec6e3683b4741ba36d9c9
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-10-19 00:24:59 +0300

    d

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">
-              Hellooooooo, I&apos;m{" "}
+              Hello, I&apos;m{" "}
             </span>
             <br />
             <TypeAnimation
